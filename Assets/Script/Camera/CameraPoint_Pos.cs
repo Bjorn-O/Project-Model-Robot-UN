c8e9e3ccf9138bd924123f32f750dfb00beee9b0
Michelle <Minoord18@outlook.com>
2021-08-11 19:35:06 +0200

    Added a script that makes the dolley track move depending on the players position

--- Assets/Script/Camera/CameraPoint_Pos.cs
+++ Assets/Script/Camera/CameraPoint_Pos.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_Movement : MonoBehaviour
+public class CameraPoint_Pos : MonoBehaviour
 {
+    public GameObject cameraPoint;
+    public GameObject player1;
+    public GameObject player2; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Camera_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        cameraPoint.transform.position = player1.transform.position + player2.transform.position / 2;
     }
 }
